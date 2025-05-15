578de10c666b4e548baa98b36b868ac86780c30b
Paul Emil Schmid <paul.e.schmid@posteo.de>
2025-05-15 22:32:35 +0200

    up

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pot-dashboard!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ pot/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pot!")
+
+
+if __name__ == "__main__":
+    main()
